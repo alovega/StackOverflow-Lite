803c7dc85afae1c70a2db722055050f3a090789f
alovega <alovegakevin@gmail.com>
2018-08-24 10:06:46 +0300

    this creates and add all the crud end points

--- app/app.py
+++ app/app.py
@@ -44,3 +44,61 @@ class QuestionList(Resource):
 
         questions.append(question)
         return {'question': questions}, 201
+
+
+
+
+class Question(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type=str, location='json')
+        self.reqparse.add_argument('description', type=str, location='json')
+        super(Question, self).__init__()
+
+    def get(self, id):
+        question = [question for question in questions if question['id'] == id]
+        if len(questions) == 0:
+            abort(404)
+        return {'question':question[0]}
+
+
+    def delete(self, id):
+        question = [question for question in questions if question['id'] == id]
+        if len(question) == 0:
+            abort(404)
+        questions.remove(question[0])
+        return {'questions': questions}
+
+
+class Answer(Question, Resource):
+    answers = []
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('answers', type=str, location='json')
+        super(Answer, self).__init__()
+
+    def post(self, id):
+        question = [question for question in questions if question['id'] == id]
+        if len(question) == 0:
+            abort(404)
+        else:
+            args = self.reqparse.parse_args()
+            self.answer = {
+                'id':self.answers[-1]['id'] + 1,
+                'answer':args['answers'],
+            }
+            self.answers.append(self.answer)
+        return {'question': question}
+
+    def put(self, id):
+        question = [question for question in questions if question['id'] == id]
+        if len(question) == 0:
+            abort(404)
+        question = question[0]
+        args = self.reqparse.parse_args()
+        for k, v in args.items():
+            for answer in v:
+                if len(v) == 0:
+                    abort(404)
+        return {'question': question}
