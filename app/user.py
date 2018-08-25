5102cb0d5446a3181cce6b0dce073ae566dd70d1
alovega <alovegakevin@gmail.com>
2018-08-25 22:41:42 +0300

    this creates the user end point and adds authorization ability of the user

---
+++ app/user.py
@@ -0,0 +1,98 @@
+from flask_restful import Resource, fields, reqparse, abort, marshal_with
+from passlib.hash import pbkdf2_sha256 as sha256
+from models.models import AppDb
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_refresh_token_required, get_jwt_identity)
+
+AppDao = AppDb()
+
+class UserApi(object):
+
+    def __init__(self,email,username,password):
+        self.email = email
+        self.username = username
+        self.password = password
+
+    @staticmethod
+    def generate_hash(password):
+        return sha256.hash(password)
+
+    @staticmethod
+    def verify_hash(password, hash):
+        return sha256.verify(password, hash)
+
+
+user_fields = {
+    'email': fields.String,
+    'username': fields.String,
+    'password': fields.String
+}
+
+
+reqparse = reqparse.RequestParser()
+reqparse.add_argument('email', type=str, required=True, help='please input email', location='json')
+reqparse.add_argument('username', type=str, required=True,help='please input username',location='json')
+reqparse.add_argument('password', type=str, required=True, help='please input password', location='json')
+
+reqparse_copy = reqparse.copy()
+reqparse_copy.remove_argument('email')
+reqparse_copy.add_argument('username', type=str, required=True, help='Invalid username', location='json')
+reqparse_copy.add_argument('password', type=str, required=True, help='Invalid password', location='json')
+
+
+class UserRegister(Resource):
+    def post(self):
+        args = reqparse.parse_args()
+        email = args['email']
+        username = args['username']
+        password = args['password']
+
+
+        if not email.replace(" ", ""):
+            return {"message":"email not valid"}
+        elif not password.replace(" ", ""):
+            return {"message":"input password"}
+        elif not username.replace(" ", ""):
+            return {"message":"input valid username"}
+
+        else:
+            user_password = UserApi.generate_hash(password)
+            AppDao.insert_user(UserApi(email=email,username=username,password=user_password))
+            return {
+                'message': 'User {0} was created'.format(username),
+            }
+
+    @marshal_with(user_fields)
+    def get(self):
+        result = AppDao.get_all()
+        return result
+
+
+class UserLogin(Resource):
+
+    def post(self):
+        args = reqparse_copy.parse_args()
+        user = AppDao.get_user_by_username(args['username'])
+
+        if not user:
+            return {"message": 'User {} doesn\'t exist'.format(args['username'])}
+
+
+        if UserApi.verify_hash(args['password'], user[0]['password']):
+            access_token = create_access_token (identity=user[0]['username'])
+            refresh_token = create_refresh_token (identity=user[0]['username'])
+            return {
+                'message': 'Logged in as {}'.format (user[0]['username']),
+                'access_token': access_token,
+                'refresh_token': refresh_token
+            }
+        else:
+            return {
+                'message': 'wrong credentials provided'
+           }
+
+class TokenRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity = current_user)
+        return {'access_token': access_token}
