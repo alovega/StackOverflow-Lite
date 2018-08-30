2d9ca2349ed2eb22c0a4c5e1d40b0c27e95c8eb0
alovega <alovegakevin@gmail.com>
2018-08-30 11:06:56 +0300

    [chore]-fixes bug to enable my navigation br to be responsive

--- UI/scripts/general.js
+++ UI/scripts/general.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 	$("a.mobile").click(function(){
 		$(".sidebar").slideToggle('fast');
@@ -10,3 +9,12 @@ $(document).ready(function(){
 		}
 	};
 });
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
