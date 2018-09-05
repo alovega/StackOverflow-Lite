a4b7e1cb63546000e31450b038b9d629473e9979
alovega <alovegakevin@gmail.com>
2018-09-05 16:34:22 +0300

    [feature][159989275]adds endpoint for posting an answer to a question

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import datetime
 from flask_restful import fields
 import json
 
-questions = [    {
+questions = [{
         'id': 1,
         'title': u'Buy groceries',
         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
@@ -17,6 +17,7 @@ questions = [    {
         'answers': [{'id':1,
                       'answer':'False'}]
     }]
+answers = []
 
 
 class QuestionDao(object):
@@ -35,8 +36,33 @@ question_fields = {
     'answers': fields.String
 }
 
+reqparse = reqparse.RequestParser()
+reqparse.add_argument('title', type=str, required=True, help='No request title provided', location='json')
+reqparse.add_argument('description', type=str, required=True, help='No request description provided', location='json')
+reqparse_copy = reqparse.copy()
+reqparse_copy.add_argument('answer', type=str, required=True, help='plase provide your answer', location='json')
+reqparse_copy.remove_argument('title')
+reqparse_copy.remove_argument('description')
+
 
 class Questions(Resource):
 
     def get(self):
         return {'qestions': questions}
+
+
+class Answer(Resource):
+
+    def post(self, id):
+        args = reqparse_copy.parse_args()
+        for question in questions:
+            if question['id'] == id:
+                answers = {
+                    'id': question['answers'][-1]['id'] + 1,
+                    'answer': args['answer'],
+                }
+                question['answers'].append(answers)
+
+                return {'answers': answers}, 201
+
+
