1cc9e61635a0633ae52bebcc75570c36c3ed536e
alovega <alovegakevin@gmail.com>
2018-09-05 20:57:05 +0300

    [#fix]fixes merge conflicts from my origin

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,9 @@
+
 from flask import Flask
 from flask_restful import Api
 from app.app import Questions
-<<<<<<< HEAD
-from app.app import Answer
-
-=======
 from app.app import Question
->>>>>>> a7d3ab658f6a0d8d749fba37c57afc3841228d30
+from app.app import Answer
 from instance.config import app_config
 
 
@@ -24,8 +21,5 @@ def create_api(app):
     api = Api(app)
 
     api.add_resource(Questions, '/questions', endpoint='questions')
-<<<<<<< HEAD
-    api.add_resource(Answer, '/answer/<int:id>', endpoint='answers')
-=======
     api.add_resource(Question, '/question/<int:id>', endpoint='question')
->>>>>>> a7d3ab658f6a0d8d749fba37c57afc3841228d30
+    api.add_resource(Answer, '/answer/<int:id>', endpoint='answer')
--- app/app.py
+++ app/app.py
@@ -1,10 +1,9 @@
-
 from flask_restful import Resource, reqparse, abort
 import datetime
 from flask_restful import fields
 import json
 
-questions = [    {
+questions = [{
         'id': 1,
         'title': u'Buy groceries',
         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
@@ -17,6 +16,7 @@ questions = [    {
         'answers': [{'id':1,
                       'answer':'False'}]
     }]
+answers = []
 
 
 class QuestionDao(object):
@@ -35,12 +35,36 @@ question_fields = {
     'answers': fields.String
 }
 
+reqparse = reqparse.RequestParser()
+reqparse.add_argument('title', type=str, required=True, help='No request title provided', location='json')
+reqparse.add_argument('description', type=str, required=True, help='No request description provided', location='json')
+reqparse_copy = reqparse.copy()
+reqparse_copy.add_argument('answer', type=str, required=True, help='plase provide your answer', location='json')
+reqparse_copy.remove_argument('title')
+reqparse_copy.remove_argument('description')
+
+def myconverter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
 
 class Questions(Resource):
 
     def get(self):
         return {'qestions': questions}
 
+    def post(self):
+        date = datetime.datetime.now()
+        args = reqparse.parse_args()
+        question = {
+            'id': questions[-1]['id'] + 1,
+            'title': args['title'],
+            'description': args['description'],
+            'date': json.dumps(date, default=myconverter)
+        }
+        questions.append(question)
+        print(question)
+        return {'question': question}, 201
 
 
 class Question(Resource):
@@ -51,3 +75,17 @@ class Question(Resource):
             abort(404)
         return {'question': question[0]}
 
+
+class Answer(Resource):
+
+    def post(self, id):
+        args = reqparse_copy.parse_args()
+        for question in questions:
+            if question['id'] == id:
+                answers = {
+                    'id': question['answers'][-1]['id'] + 1,
+                    'answer': args['answer'],
+                }
+                question['answers'].append(answers)
+
+                return {'answers': answers}, 201
